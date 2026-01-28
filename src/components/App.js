9bce0062975b040495012a8c8039710e91f036dd
sowndarya1208-pixel <sowndarya1208@gmail.com>
2026-01-28 21:36:48 +0530

    Add counter functionality to App component
    Refactor App component to include a counter and button.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
+
         {/* Do not remove the main div */}
-    <h1>sowndarya</h1>
+    function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+       <div>
+        <h1>Button clicked {count} times</h1>
+        <button onClick={() => setCount((count) => count + 1)}>
+          onClick me
+        </button>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
     </div>
   )
 }
