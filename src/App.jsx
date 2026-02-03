d3844e2788f29c9276798e1a4ce081f7e9dbefa1
sowndarya <sowndarya1208@gmail.com>
2026-02-03 22:33:18 +0530

    Button counter assignment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Button clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
